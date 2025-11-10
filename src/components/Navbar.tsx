f39894c333c7168827a1cec058a97768b52425e0
LENOVO <anjanaheshan676@gmail.com>
2025-11-10 18:50:52 +0530

    nav bar updated profile pic

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
     const [showMenu , setShowMenu] = useState(false)
     // const [token , setToken] = useState(true)
-    const { token , setToken} = useContext(AppContext)
+    const { token , setToken, userData} = useContext(AppContext)
     const [showDropdown, setShowDropdown] = useState(false)
 
     const logout = () => {
@@ -43,10 +43,10 @@ const Navbar = () => {
         </ul>
         <div className='flex items-center gap-4'>
             {
-                token 
+                token && userData
                 ? 
                 <div onClick={() => setShowDropdown(!showDropdown)} className='flex items-center gap-2 cursor-pointer group relative'>
-                    <img className='w-8 rounded-full' src={assets.profile_pic} alt="Profile picture" />
+                    <img className='w-8 rounded-full' src={userData.image} alt="Profile picture" />
                     <img className='w-2.5' src={assets.dropdown_icon} alt="Drop down icon" />
                     <div className={`absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 ${showDropdown ? 'block' : 'hidden'} group-hover:block`}>
                         <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
